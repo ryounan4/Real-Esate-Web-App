0f4b5fed31f8b6c8b7568c2f2635e1386cddb5b7
Ryan Younan <ryanyounan@gmail.com>
2025-01-22 16:11:27 -0800

    Added a bunch of new styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
 
                 <img src='./logo.png' alt='logo' width={100}/>
 
-                <div className="h-menu">
+                <div className="flexCenter h-menu">
                     <a href="">Residencies</a>
                     <a href="">Our Values</a>
                     <a href="">Contact Us</a>
